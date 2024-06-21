0eec906b3357253dcfbed82f587abfeee23609f2
Edward Fransindani <efransindani@gmail.com>
2024-06-21 15:46:07 +0700

    feat: add redsync and locker manager

--- go.mod
+++ go.mod
@@ -4,8 +4,6 @@ go 1.13
 
 require (
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redsync/redsync/v4 v4.13.0
 	github.com/joho/godotenv v1.3.0
-	github.com/onsi/ginkgo v1.14.2 // indirect
-	github.com/onsi/gomega v1.10.4 // indirect
-	golang.org/x/text v0.3.4 // indirect
 )
---
+++ locker/locker.go
@@ -0,0 +1,74 @@
+package locker
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/edu3dw4rd/redis"
+
+	"github.com/go-redsync/redsync/v4"
+	"github.com/go-redsync/redsync/v4/redis/goredis"
+)
+
+var (
+	lockerManager *LockerManager
+)
+
+type LockerManager struct {
+	redisSync *redsync.Redsync
+}
+
+func NewLockerManager() *LockerManager {
+	if lockerManager == nil || lockerManager.redisSync == nil {
+		// Get redis client
+		rdsClient := redis.GetRedisClient()
+
+		pool := goredis.NewPool(rdsClient)
+		lockerManager = &LockerManager{
+			redisSync: redsync.New(pool),
+		}
+	}
+
+	return lockerManager
+}
+
+func (l *LockerManager) ObtainLock(key string, retryNum int, ttl, retryDelay time.Duration) (*redsync.Mutex, error) {
+	var mutex *redsync.Mutex
+	options := l.setRedsyncOptions(retryNum, ttl, retryDelay)
+
+	if len(options) > 0 {
+		mutex = l.redisSync.NewMutex(key, options...)
+	} else {
+		mutex = l.redisSync.NewMutex(key)
+	}
+
+	if err := mutex.Lock(); err != nil {
+		return nil, fmt.Errorf("failed to obtain lock [%s]: %s", mutex.Name, err.Error())
+	}
+
+	return mutex, nil
+}
+
+func (l *LockerManager) ReleaseLock(mutex *redsync.Mutex) error {
+	if ok, err := mutex.Unlock(); !ok || err != nil {
+		return fmt.Errorf("failed to release lock [%s]: %s", mutex.Name, err.Error())
+	}
+
+	return nil
+}
+
+func (l *LockerManager) setRedsyncOptions(retryNum int, ttl, retryDelay time.Duration) (options []redsync.Option) {
+	if retryNum > 0 {
+		options = append(options, redsync.WithTries(retryNum))
+	}
+
+	if ttl > 0 {
+		options = append(options, redsync.WithExpiry(ttl))
+	}
+
+	if retryDelay > 0 {
+		options = append(options, redsync.WithRetryDelay(retryDelay))
+	}
+
+	return
+}
